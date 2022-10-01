abc1ee3b5b5d3c7842d3559c07765c5011ab69a2
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-02 02:57:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,17 @@ import "../styles/App.css";
 class App extends React.Component {
   handleClick() {
     this.setState({ count: this.state.count + 1 });
-    console.log("mounted");
   }
+
   constructor(props) {
     super(props);
     this.state = { count: 0 };
     this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      console.log("mounted");
+    }, 1000);
   }
 
   render() {
